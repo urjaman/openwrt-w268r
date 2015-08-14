732d4643d272b71a313dcebf831a523c86add3cd
Urja Rannikko <urjaman@gmail.com>
2015-08-14 21:35:28 +0300

    remove support for pivoting roots of any kind (we're initramfs _only_)

--- package/base-files/files/lib/functions/preinit.sh
+++ package/base-files/files/lib/functions/preinit.sh
@@ -65,23 +65,10 @@ boot_run_hook() {
 }
 
 pivot() { # <new_root> <old_root>
-	/bin/mount -o noatime,move /proc $1/proc && \
-	pivot_root $1 $1$2 && {
-		/bin/mount -o noatime,move $2/dev /dev
-		/bin/mount -o noatime,move $2/tmp /tmp
-		/bin/mount -o noatime,move $2/sys /sys 2>&-
-		/bin/mount -o noatime,move $2/overlay /overlay 2>&-
-		return 0
-	}
 }
 
 fopivot() { # <rw_root> <ro_root> <dupe?>
-	/bin/mount -o noatime,lowerdir=/,upperdir=$1 -t overlayfs "overlayfs:$1" /mnt
-	pivot /mnt $2
 }
 
 ramoverlay() {
-	mkdir -p /tmp/root
-	/bin/mount -t tmpfs -o noatime,mode=0755 root /tmp/root
-	fopivot /tmp/root /rom 1
 }
