98771aa98b70e20b4da295f9cbc77c39efde0dd4
Urja Rannikko <urjaman@gmail.com>
2019-06-14 11:11:53 +0000

    Add a tiny base files package to be used with the W268R
    The point of this duplication is to reduce the amount of noise
    (merge conflicts) caused by nonrelevant changes to base-files when
    I'm updating my tree.
    
    also: make generate_config possible to configure out
    
    it's kinda useful on first boot -
    that you can TFTP load to SRAM anyways so size matters less -
    but useless afterwards ... about 1kB reduction in lzma size tho :/.

---
+++ package/base-files-tiny/files/lib/functions/migrations.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+migrate_led_sysfs() {
+	return
+}
+
+migrate_leds() {
+	return
+}
+
+migrations_apply() {
+	return
+}
---
+++ package/base-files-tiny/files/lib/functions/preinit.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+# Copyright (C) 2006-2013 OpenWrt.org
+# Copyright (C) 2010 Vertical Communications
+
+boot_hook_splice_start() {
+	export -n PI_HOOK_SPLICE=1
+}
+
+boot_hook_splice_finish() {
+	local hook
+	for hook in $PI_STACK_LIST; do
+		local v; eval "v=\${${hook}_splice:+\$${hook}_splice }$hook"
+		export -n "${hook}=${v% }"
+		export -n "${hook}_splice="
+	done
+	export -n PI_HOOK_SPLICE=
+}
+
+boot_hook_init() {
+	local hook="${1}_hook"
+	export -n "PI_STACK_LIST=${PI_STACK_LIST:+$PI_STACK_LIST }$hook"
+	export -n "$hook="
+}
+
+boot_hook_add() {
+	local hook="${1}_hook${PI_HOOK_SPLICE:+_splice}"
+	local func="${2}"
+
+	[ -n "$func" ] && {
+		local v; eval "v=\$$hook"
+		export -n "$hook=${v:+$v }$func"
+	}
+}
+
+boot_hook_shift() {
+	local hook="${1}_hook"
+	local rvar="${2}"
+
+	local v; eval "v=\$$hook"
+	[ -n "$v" ] && {
+		local first="${v%% *}"
+
+		[ "$v" != "${v#* }" ] && \
+			export -n "$hook=${v#* }" || \
+			export -n "$hook="
+
+		export -n "$rvar=$first"
+		return 0
+	}
+
+	return 1
+}
+
+boot_run_hook() {
+	local hook="$1"
+	local func
+
+	while boot_hook_shift "$hook" func; do
+		local ran; eval "ran=\$PI_RAN_$func"
+		[ -n "$ran" ] || {
+			export -n "PI_RAN_$func=1"
+			$func "$1" "$2"
+		}
+	done
+}
+
+pivot() { # <new_root> <old_root>
+	return
+}
+
+fopivot() { # <rw_root> <work_dir> <ro_root> <dupe?>
+	return
+}
+
+ramoverlay() {
+	return
+}
